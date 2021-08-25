0c2465f6be939da9c3d57dde5d6a9cac61bfd4d5
Ryan Feigenbaum <director@philsci.org>
2021-08-25 17:54:43 -0400

    new build

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/climate-change-survey",
   build: {
     outDir: "../docs",
-    assetsDir: "climate-change-survey",
   },
 });
